4a5acf599dc8ad0e98214443b3ae0a411c62b15f
Jay Chakravarty <jaychakravarty@icloud.com>
2024-05-29 14:18:30 -0700

    lesson4-challenge1

---
+++ lesson04/exercise-one.js
@@ -0,0 +1,8 @@
+arr = ["string0", "string1", "string2", "string3", "string4"];
+console.log(arr);
+arr.push("string5");
+console.log(arr);
+arr.splice(2, 1);
+console.log(arr);
+string = arr.join(", ");
+console.log(string);
